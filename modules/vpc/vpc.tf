dbb948b1942e108bac3f011bab3ef129e5720789
seanlucey <36920610+seanlucey@users.noreply.github.com>
2021-11-18 11:33:59 +0000

    Update vpc.tf

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,1 +1,9 @@
-resource "aws_vpc" "main"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main"
+  }
+}
