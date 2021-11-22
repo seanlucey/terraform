aa52c0fa9cca406830078ac44db11371ffea4942
seanlucey <36920610+seanlucey@users.noreply.github.com>
2021-11-22 17:08:07 +0000

    Update subnets.tf

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -5,26 +5,26 @@ locals {
 }
 
 resource "aws_subnet" "sn_a" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = local.cidr_block_a
   availability_zone = "${var.AWS_REGION}a"
   tags = {
-    Name = "Main_Subnet_A"
+    Name = "${var.vpc_name}_Subnet_A"
   }
 }
 resource "aws_subnet" "sn_b" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = local.cidr_block_b
   availability_zone = "${var.AWS_REGION}b"
   tags = {
-    Name = "Main_Subnet_B"
+    Name = "${var.vpc_name}_Subnet_B"
   }
 }
 resource "aws_subnet" "sn_c" {
-  vpc_id     = aws_vpc.main.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = local.cidr_block_c
   availability_zone = "${var.AWS_REGION}c"
   tags = {
-    Name = "Main_Subnet_C"
+    Name = "${var.vpc_name}_Subnet_C"
   }
 }
