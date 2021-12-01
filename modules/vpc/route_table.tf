0adf5dca4952e53c3c465683ddaca17fd1c22d63
seanlucey <36920610+seanlucey@users.noreply.github.com>
2021-12-01 15:28:13 +0000

    Update route_table.tf

--- modules/vpc/route_table.tf
+++ modules/vpc/route_table.tf
@@ -5,12 +5,6 @@ resource "aws_default_route_table" "public_route_table" {
     cidr_block = "${var.vpc_cidr_block}"
     gateway_id = aws_internet_gateway.igw.id
   }
-
-  route {
-    ipv6_cidr_block        = "::/0"
-    egress_only_gateway_id = aws_egress_only_internet_gateway.igw.id
-  }
-
   tags = {
     Name = "Route Table"
   }
