bbf8dd087fb55d73264ed43ccf5d25a2370235ba
seanlucey <36920610+seanlucey@users.noreply.github.com>
2021-12-07 19:07:20 +0000

    Create vpc.tf

---
+++ aws/modules/vpc/vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "vpc" {
+  cidr_block       = cidrsubnet(var.vpc_cidr_block, 0, 0)
+  instance_tenancy = "default"
+  enable_dns_hostnames = var.vpc_enable_dns_hostnames
+
+  tags = {
+    Name = var.vpc_name != "" ? "${lower(var.name)}-vpc"
+  }
+}
