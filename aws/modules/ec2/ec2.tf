2dce06051e362049a4ee8b07d9291c38be5bc086
seanlucey <36920610+seanlucey@users.noreply.github.com>
2022-03-23 18:30:40 +0000

    Update ec2.tf

--- aws/modules/ec2/ec2.tf
+++ aws/modules/ec2/ec2.tf
@@ -1,49 +1,123 @@
-data "aws_vpc" "vpc" {
-  filter {
-    name = "tag:Name"
-    values = ["main-vpc"]
-  }
-}
-data "aws_ami" "packer_ami" {
-  filter {
-      name = "tag:Name"
-      values = ["packer"]
-  }
-}
-data "aws_subnet" "subnet" {
-  filter {
-      name = "tag:Name"
-      values = ["${var.instance_subnet}"]
-  }
-}
 resource "aws_instance" "ec2" {
-        ami = data.aws_ami.packer_ami.id
-        instance_type = var.instance_type
-        associate_public_ip_address = true
-        availability_zone = var.instance_availability_zone
-        disable_api_termination = true
-        key_name = var.instance_key_name
-        subnet_id = data.aws_subnet.subnet.id
-        vpc_security_group_ids = [
-                                    data.aws_security_group.home_ssh.id,
-                                    data.aws_security_group.sql.id
-                                  ]
-        tags = {
-                Name = "${lower(var.instance_name)}"
-        }
-}
+  ami                         = "${var.ami}"
+  instance_type               = "${var.instance_type}"
+  associate_public_ip_address = "${var.instance_associate_public_ip}"
+  availability_zone           = "${var.availability_zone}"
+  disable_api_termination     = true
+  key_name                    = "${var.key_name}"
+  subnet_id                   = "${var.subnet_id}"
+  vpc_security_group_ids      = "${var.vpc_security_group_ids}"
 
-resource "aws_eip" "eip" {
-  vpc = true
-  instance = aws_instance.ec2.id
-  tags = {
-                Name = "${upper(var.instance_name)}"
+  get_password_data           = "${var.get_password_data}"
+  iam_instance_profile        = "${var.iam_instance_profile}"
+
+  ebs_optimized = var.ebs_optimized
+
+  dynamic "capacity_reservation_specification" {
+    for_each = var.capacity_reservation_specification != null ? [var.capacity_reservation_specification] : []
+    content {
+      capacity_reservation_preference = lookup(capacity_reservation_specification.value, "capacity_reservation_preference", null)
+
+      dynamic "capacity_reservation_target" {
+        for_each = lookup(capacity_reservation_specification.value, "capacity_reservation_target", [])
+        content {
+          capacity_reservation_id = lookup(capacity_reservation_target.value, "capacity_reservation_id", null)
         }
+      }
+    }
+  }
+
+  dynamic "root_block_device" {
+    for_each = var.root_block_device
+    content {
+      delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
+      encrypted             = lookup(root_block_device.value, "encrypted", null)
+      iops                  = lookup(root_block_device.value, "iops", null)
+      kms_key_id            = lookup(root_block_device.value, "kms_key_id", null)
+      volume_size           = lookup(root_block_device.value, "volume_size", null)
+      volume_type           = lookup(root_block_device.value, "volume_type", null)
+      throughput            = lookup(root_block_device.value, "throughput", null)
+      tags                  = lookup(root_block_device.value, "tags", null)
+    }
+  }
+
+  dynamic "ebs_block_device" {
+    for_each = var.ebs_block_device
+    content {
+      delete_on_termination = lookup(ebs_block_device.value, "delete_on_termination", null)
+      device_name           = ebs_block_device.value.device_name
+      encrypted             = lookup(ebs_block_device.value, "encrypted", null)
+      iops                  = lookup(ebs_block_device.value, "iops", null)
+      kms_key_id            = lookup(ebs_block_device.value, "kms_key_id", null)
+      snapshot_id           = lookup(ebs_block_device.value, "snapshot_id", null)
+      volume_size           = lookup(ebs_block_device.value, "volume_size", null)
+      volume_type           = lookup(ebs_block_device.value, "volume_type", null)
+      throughput            = lookup(ebs_block_device.value, "throughput", null)
+    }
+  }
+
+  dynamic "ephemeral_block_device" {
+    for_each = var.ephemeral_block_device
+    content {
+      device_name  = ephemeral_block_device.value.device_name
+      no_device    = lookup(ephemeral_block_device.value, "no_device", null)
+      virtual_name = lookup(ephemeral_block_device.value, "virtual_name", null)
+    }
+  }
+
+  dynamic "metadata_options" {
+    for_each = var.metadata_options != null ? [var.metadata_options] : []
+    content {
+      http_endpoint               = lookup(metadata_options.value, "http_endpoint", "enabled")
+      http_tokens                 = lookup(metadata_options.value, "http_tokens", "optional")
+      http_put_response_hop_limit = lookup(metadata_options.value, "http_put_response_hop_limit", "1")
+      instance_metadata_tags      = lookup(metadata_options.value, "instance_metadata_tags", null)
+    }
+  }
+
+  dynamic "network_interface" {
+    for_each = var.network_interface
+    content {
+      device_index          = network_interface.value.device_index
+      network_interface_id  = lookup(network_interface.value, "network_interface_id", null)
+      delete_on_termination = lookup(network_interface.value, "delete_on_termination", false)
+    }
+  }
+
+  dynamic "launch_template" {
+    for_each = var.launch_template != null ? [var.launch_template] : []
+    content {
+      id      = lookup(var.launch_template, "id", null)
+      name    = lookup(var.launch_template, "name", null)
+      version = lookup(var.launch_template, "version", null)
+    }
+  }
+
+  enclave_options {
+    enabled = var.enclave_options_enabled
+  }
+
+  source_dest_check                    = length(var.network_interface) > 0 ? null : var.source_dest_check
+  tenancy                              = var.tenancy
+  host_id                              = var.host_id
+
+  credit_specification {
+    cpu_credits = null
+  }
+
+  timeouts {
+    create = lookup(var.timeouts, "create", null)
+    update = lookup(var.timeouts, "update", null)
+    delete = lookup(var.timeouts, "delete", null)
+  }
+
+  tags        = merge({ "Name" = var.name}, var.tags)
+  volume_tags = var.enable_volume_tags ? merge({ "Name" = var.name }, var.volume_tags) : null
 }
 resource "aws_route53_record" "ec2_privateip" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
-  type    = "A"
+  zone_id = var.zone_id
+  name    = var.dns_name
+  type    = var.record_type
   ttl     = "300"
-  records = [aws_eip.eip.public_ip]
+  records = [aws_instance.ec2.private_ip]
 }
