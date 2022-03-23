534cc4c78c03cfde86f1bf0dd72765f4dca61c7b
seanlucey <36920610+seanlucey@users.noreply.github.com>
2022-03-23 18:24:40 +0000

    Update variables.tf

--- aws/modules/vpc/variables.tf
+++ aws/modules/vpc/variables.tf
@@ -1,12 +1,48 @@
-variable "AWS_REGION" {
-    description = "(Optional, default 'eu-west-1') The region where to deploy this code."
-    default = "eu-west-1"
+resource "aws_route_table" "private" {
+  count  = local.max_subnet_length
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}_private_route_table_${count.index}"
+  })
 }
-variable "name" {
-  description = "(Required) The name of the vpc"
-  type        = string
+
+resource "aws_route" "private_nat_gateway" {
+  count                  = local.max_subnet_length
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.nat_gw.*.id, count.index)
+
+  timeouts {
+    create = "5m"
+  }
 }
-variable "cidr_block" {
-  description = "(Optional) The IPv4 CIDR block for the VPC."
-  type        = string
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private.*)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}_public_route_table"
+  })
+}
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public.*)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+
+  timeouts {
+    create = "5m"
+  }
 }
