60834b2073632c89d9f87d33a2f77c9d88e1975f
seanlucey <36920610+seanlucey@users.noreply.github.com>
2022-05-07 19:13:14 +0100

    Update route_table.tf

--- aws/modules/vpc/route_table.tf
+++ aws/modules/vpc/route_table.tf
@@ -1,11 +1,48 @@
-resource "aws_default_route_table" "public_route_table" {
-  default_route_table_id = aws_vpc.vpc.default_route_table_id
+resource "aws_route_table" "private" {
+  count  = local.max_subnet_length
+  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "${var.vpc_cidr_block}"
-    gateway_id = aws_internet_gateway.igw.id
+  tags = merge(local.common_tags, {
+    Name = var.name_private_route_table_${count.index}"
+  })
+}
+
+resource "aws_route" "private_nat_gateway" {
+  count                  = local.max_subnet_length
+  route_table_id         = element(aws_route_table.private.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = element(aws_nat_gateway.nat_gw.*.id, count.index)
+
+  timeouts {
+    create = "5m"
   }
-  tags = {
-    Name = "Route Table"
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private.*)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(local.common_tags, {
+    Name = var.name_public_route_table"
+  })
+}
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public.*)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+
+  timeouts {
+    create = "5m"
   }
 }
