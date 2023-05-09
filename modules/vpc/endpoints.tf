c27d41e4a6a1bf1900fa2daa300acade15feb1ce
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-09 12:01:45 +0100

    Update endpoints.tf

--- modules/vpc/endpoints.tf
+++ modules/vpc/endpoints.tf
@@ -1,8 +1,7 @@
 data "aws_region" "current_region" {}
 
 resource "aws_vpc_endpoint" "s3_endpoint" {
-    count = var.s3_endpoint_enable ? 1 : 0
-
+    
     vpc_id = aws_vpc.main.id
     vpc_endpoint_type = "Gateway"
     service_name      = "com.amazonaws.${data.aws_region.current_region.name}.s3"
