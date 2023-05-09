5e4292de054b1f5920bec26bae50e40679852dbc
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-09 17:18:20 +0100

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -16,3 +16,20 @@ resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
     mfa_delete = try(tobool(var.versioning["mfa_delete"]) ? "Enabled" : "Disabled", title(lower(var.versioning["mfa_delete"])), null)
   }
 }
+
+resource "aws_s3_bucket_cors_configuration" "s3_bucket_cors" {
+  bucket                = aws_s3_bucket.s3_bucket.id
+
+  dynamic "cors_rule" {
+    for_each = local.cors_rules
+
+    content {
+      id              = try(cors_rule.value.id, null)
+      allowed_methods = cors_rule.value.allowed_methods
+      allowed_origins = cors_rule.value.allowed_origins
+      allowed_headers = try(cors_rule.value.allowed_headers, null)
+      expose_headers  = try(cors_rule.value.expose_headers, null)
+      max_age_seconds = try(cors_rule.value.max_age_seconds, null)
+    }
+  }
+}
