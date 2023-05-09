1c61ea89cf560a6d958cfc9d17ff1e144e542a8e
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-09 16:16:55 +0100

    Update locals.tf

--- modules/s3/locals.tf
+++ modules/s3/locals.tf
@@ -3,7 +3,7 @@ locals {
         Terraform             = "true"
         Environment           = var.environment
     }
-    region = data.aws_region.current.name
+    aws_region = data.aws_region.current.name
 }
 
 data "aws_region" "current" {}
