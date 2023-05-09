dce8c082c4b90d200c667226fde9ee5c6adbf1f4
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-09 14:16:21 +0100

    Update locals.tf

--- modules/vpc/locals.tf
+++ modules/vpc/locals.tf
@@ -6,10 +6,6 @@ locals {
     zone_names        = data.aws_availability_zones.available.names
     max_subnet_length = length(local.zone_names)
     nat_gateway_ips   = tolist(aws_eip.nat.*.id)
-
-    cidr_block_a  = cidrsubnet(aws_vpc.main.cidr_block, 2, 0)
-    cidr_block_b = cidrsubnet(aws_vpc.main.cidr_block, 2, 1)
-    cidr_block_c = cidrsubnet(aws_vpc.main.cidr_block, 2, 2)
 }
 
 data "aws_availability_zones" "available" {
