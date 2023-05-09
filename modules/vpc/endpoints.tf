a536b3db9302a30fc4c0d390c174c6d030a339c0
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-09 12:05:22 +0100

    Update endpoints.tf

--- modules/vpc/endpoints.tf
+++ modules/vpc/endpoints.tf
@@ -5,7 +5,7 @@ resource "aws_vpc_endpoint" "s3_endpoint" {
     vpc_id = aws_vpc.main.id
     vpc_endpoint_type = "Gateway"
     service_name      = "com.amazonaws.${data.aws_region.current_region.name}.s3"
-    route_table_ids = ["${aws_route_table.private_route_table.id}"]
+    route_table_ids = ["${aws_route_table.private_route_table.*.id}"]
 }
 
 /*resource "aws_vpc_endpoint_route_table_association" "s3_endpoint" {
