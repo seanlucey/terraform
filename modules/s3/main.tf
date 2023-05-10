02772e2f67ae0747976b7b631d91b142bc2aee57
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-10 17:19:16 +0100

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -14,3 +14,26 @@ resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
     status = try(var.versioning["enabled"] ? "Enabled" : "Suspended", tobool(var.versioning["status"]) ? "Enabled" : "Suspended", title(lower(var.versioning["status"])))
   }
 }
+
+resource "aws_s3_bucket_replication_configuration" "s3_bucket_crr" {
+  count = local.create_crr_bucket ? 1 : 0
+  
+  bucket = aws_s3_bucket.s3_bucket.id
+  role = var.replication_configuration
+  
+  dynamic "destination" {
+    content {
+      bucket = destination.value.bucket
+    }
+/*     access_control_translation {
+        owner = "Destination"
+      }
+    
+    dynamic "encryption_configuration" {
+     content {
+              replica_kms_key_id = encryption_configuration.value
+            }
+          }*/
+
+  depends_on = [aws_s3_bucket_versioning.s3_bucket_versioning]  
+}
