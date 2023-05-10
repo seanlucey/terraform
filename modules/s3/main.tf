3e20660d2d96688f6063551668b36483e8abec91
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-10 21:24:37 +0100

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
 resource "aws_s3_bucket_replication_configuration" "s3_bucket_crr" {
   
   bucket = aws_s3_bucket.s3_bucket.id
-  role = aws_iam_role.replication[0].arn
+  role = aws_iam_role.replication.arn
   
   dynamic "rule" {
     for_each = flatten(try([var.replication_configuration["rule"]], [var.replication_configuration["rules"]], []))
