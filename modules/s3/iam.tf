24576f55ba131ee162fa85e5a7784ed799dc1818
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-10 20:52:59 +0100

    Update iam.tf

--- modules/s3/iam.tf
+++ modules/s3/iam.tf
@@ -1,7 +1,11 @@
+resource "random_id" "random" {
+  byte_length = 8
+}
+
 resource "aws_iam_role" "replication" {
   count = local.create_crr_bucket ? 1 : 0
 
-  name = "s3-bucket-replication-${random_pet.this.id}"
+  name = "s3-bucket-replication-${random_id.random.hex}"
 
   assume_role_policy = <<POLICY
 {
