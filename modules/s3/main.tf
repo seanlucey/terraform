37ba657eef6bd1ca31ed4458460cbca8959802b2
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-10 07:55:47 +0100

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -9,27 +9,8 @@ resource "aws_s3_bucket" "s3_bucket" {
 
 resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
   bucket = aws_s3_bucket.s3_bucket.id
-  mfa = try(var.versioning["mfa"], null)
 
   versioning_configuration {
-    status = try(var.versioning["enabled"] ? "Enabled" : "Suspended", tobool(var.versioning["status"]) ? "Enabled" : "Suspended", title(lower(var.versioning["status"])))
-    mfa_delete = try(tobool(var.versioning["mfa_delete"]) ? "Enabled" : "Disabled", title(lower(var.versioning["mfa_delete"])), null)
-  }
-}
-
-resource "aws_s3_bucket_cors_configuration" "s3_bucket_cors" {
-  bucket                = aws_s3_bucket.s3_bucket.id
-
-  dynamic "cors_rule" {
-    for_each = local.cors_rules
-
-    content {
-      id              = try(cors_rule.value.id, null)
-      allowed_methods = cors_rule.value.allowed_methods
-      allowed_origins = cors_rule.value.allowed_origins
-      allowed_headers = try(cors_rule.value.allowed_headers, null)
-      expose_headers  = try(cors_rule.value.expose_headers, null)
-      max_age_seconds = try(cors_rule.value.max_age_seconds, null)
-    }
+    status = try(var.versioning["enabled"] ? "Enabled" : "Suspended")
   }
 }
