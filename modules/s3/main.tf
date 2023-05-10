49d0da5bdb878a912bb02b3291db678b94a0d2e5
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-10 21:28:19 +0100

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -16,6 +16,7 @@ resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
 }
 
 resource "aws_s3_bucket_replication_configuration" "s3_bucket_crr" {
+  count = length(keys(var.replication_configuration)) > 0 ? 1 : 0
   
   bucket = aws_s3_bucket.s3_bucket.id
   role = aws_iam_role.replication.arn
