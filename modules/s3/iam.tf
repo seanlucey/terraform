bacb399328a17397535498a55895388fd7f9fed3
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-10 20:48:32 +0100

    Update iam.tf

--- modules/s3/iam.tf
+++ modules/s3/iam.tf
@@ -1,1 +1,19 @@
+resource "aws_iam_role" "replication" {
+  name = "s3-bucket-replication-${random_pet.this.id}"
 
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
