5f6caaffc8aea720f5d32c86eb249f9448cf3ab2
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-11 10:32:31 +0100

    Update cross-region-replication.tf

--- modules/s3/cross-region-replication.tf
+++ modules/s3/cross-region-replication.tf
@@ -49,5 +49,5 @@ resource "aws_s3_bucket_replication_configuration" "s3_bucket_crr" {
 
     }
   }
-  depends_on = [aws_s3_bucket_versioning.this]
+  depends_on = [aws_s3_bucket_versioning.s3_bucket_versioning]
 }
