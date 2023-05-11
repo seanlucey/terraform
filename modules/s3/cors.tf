d3ec7d536a7175878283598b9e0e2d31c704802f
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-11 11:55:01 +0100

    Create cors.tf

---
+++ modules/s3/cors.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket_cors_configuration" "s3_cors" {
+  count = length(local.cors_rules) > 0 ? 1 : 0
+
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  dynamic "cors_rule" {
+    for_each = local.cors_rules
+
+    content {
+      id              = try(cors_rule.value.id, null)
+      allowed_methods = cors_rule.value.allowed_methods
+      allowed_origins = cors_rule.value.allowed_origins
+      allowed_headers = try(cors_rule.value.allowed_headers, null)
+      expose_headers  = try(cors_rule.value.expose_headers, null)
+      max_age_seconds = try(cors_rule.value.max_age_seconds, null)
+    }
+  }
+}
