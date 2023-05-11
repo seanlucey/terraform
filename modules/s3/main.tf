9f16e6b44fa219085dcbd6983a2b9511a4d4d54a
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-11 11:53:09 +0100

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -6,11 +6,3 @@ resource "aws_s3_bucket" "s3_bucket" {
     Name = "${var.bucket}-${var.environment}-${local.aws_region}"
   })
 }
-
-resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  versioning_configuration {
-    status = try(var.versioning["enabled"] ? "Enabled" : "Suspended", tobool(var.versioning["status"]) ? "Enabled" : "Suspended", title(lower(var.versioning["status"])))
-  }
-}
