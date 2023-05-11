7557759457b3c36d91bfca99f9119e0a882c4e12
seanlucey <36920610+seanlucey@users.noreply.github.com>
2023-05-11 13:37:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "s3" {
       allowed_methods = ["GET"]
       allowed_origins = ["https://alpha.activedraft.com", "https://blazorapp.alpha.activedraft.com"]
       allowed_headers = ["*"]
-      expose_headers  = [""Connection", "Etag", "Retry-After", "X-Amz-Bucket-Region", "Expires", "Content-Range", "Content-Language", "Date", "Cache-Control", "Last-Modified", "Content-Encoding", "Content-Disposition", "Accept-Ranges", "Server", "Content-Type", "Content-Length""]
+      expose_headers  = ["Connection", "Etag", "Retry-After", "X-Amz-Bucket-Region", "Expires", "Content-Range", "Content-Language", "Date", "Cache-Control", "Last-Modified", "Content-Encoding", "Content-Disposition", "Accept-Ranges", "Server", "Content-Type", "Content-Length"]
       max_age_seconds = 3000
       }, {
       allowed_methods = ["POST", "HEAD", "DELETE", "PUT"]
